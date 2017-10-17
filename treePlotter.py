cdf89b4075b9ac47777fb1994adcd0a2d1867b22
wangwen <wangwen@wangwendeMacBook-Air.local>
2017-10-18 03:17:59 +0800

    fix some error about plot tree

--- treePlotter.py
+++ treePlotter.py
@@ -35,8 +35,8 @@ def getTreeDepth(myTree):
             thisDepth=1+getTreeDepth(secondDict[key])
         else:
             thisDepth=1
-    if thisDepth>maxDepth:
-        maxDepth=thisDepth
+        if thisDepth>maxDepth:
+            maxDepth=thisDepth
     return maxDepth
 def retrieveTree(i):
     listOfTrees =[{'no surfacing': {0: 'no', 1: {'flippers': \
@@ -45,3 +45,35 @@ def retrieveTree(i):
                     {0: {'head': {0: 'no', 1: 'yes'}}, 1: 'no'}}}}
                   ]
     return listOfTrees[i]
+def plotMidText(centerPt,parentPt,txtString):
+    midX=(parentPt[0]-centerPt[0])/2+centerPt[0]
+    midY=(parentPt[1]-centerPt[1])/2+centerPt[1]
+    createPlot.ax1.text(midX,midY,txtString)
+def plotTree(myTree,parentPt,nodeTxt):
+    leafsNum=getLeafsNum(myTree)
+    treeDepth=getTreeDepth(myTree)
+    firstStr=list(myTree.keys())[0]
+    centerPt=(plotTree.xoff+(1+float(leafsNum))/2/plotTree.totalW,plotTree.yoff)
+    plotMidText(centerPt,parentPt,nodeTxt)
+    plotNode(firstStr,centerPt,parentPt,decisionNode)
+    secondDict=myTree[firstStr]
+    plotTree.yoff=plotTree.yoff-1.0/plotTree.totalD
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__=='dict':
+            plotTree(secondDict[key],centerPt,str(key))
+        else:
+            plotTree.xoff=plotTree.xoff+1.0/plotTree.totalW
+            plotNode(secondDict[key],(plotTree.xoff,plotTree.yoff),centerPt,leafNode)
+            plotMidText((plotTree.xoff,plotTree.yoff),centerPt,str(key))
+    plotTree.yoff=plotTree.yoff+1/plotTree.totalD
+def createPlot(inTree):
+    fig=plt.figure(1,facecolor='white')
+    fig.clf()
+    axprops=dict(xticks=[],yticks=[])
+    createPlot.ax1=plt.subplot(111,frameon=False,**axprops)
+    plotTree.totalW=float(getLeafsNum(inTree))
+    plotTree.totalD=float(getTreeDepth(inTree))
+    plotTree.xoff=-0.5/plotTree.totalW
+    plotTree.yoff=1.0
+    plotTree(inTree,(0.5,1.0),'')
+    plt.show()
