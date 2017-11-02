64d70b1f95a24c1acde5c97411963248c207d866
Ankerhu <Ankerhu@MacdeMacBook-Pro.local>
2017-11-02 11:50:59 +0800

    prepare spamTest

--- bayes.py
+++ bayes.py
@@ -27,15 +27,22 @@ def setOfWord2Vec(vocabList,inputSet):
         else:
             print('the word %d is not in my vocabulary!' % word)
     return returnVec
-            
+
+def bagOfWords2VecMN(vocabList,inputSet):
+    returnVec=[0]*len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] += 1
+    return returnVec
+    
 def trainNB0(trainMatrix,trainCategory):
     numTrainDocs=len(trainMatrix)
     numWords=len(trainMatrix[0])
     pAbusive=sum(trainCategory)/numTrainDocs
-    p0Num=zeros(numWords)
-    p1Num=zeros(numWords)
-    p0Denom=0.0
-    p1Denom=0.0
+    p0Num=ones(numWords)
+    p1Num=ones(numWords)
+    p0Denom=2.0
+    p1Denom=2.0
     for i in range(numTrainDocs):
         if trainCategory[i] == 1:
             p1Num += trainMatrix[i]
@@ -43,6 +50,27 @@ def trainNB0(trainMatrix,trainCategory):
         else:
             p0Num += trainMatrix[i]
             p0Denom += sum(trainMatrix[i])
-    p1Vect=p1Num/p1Denom
-    p0Vect=p0Num/p0Denom
+    p1Vect=log(p1Num/p1Denom)
+    p0Vect=log(p0Num/p0Denom)
     return p0Vect,p1Vect,pAbusive
+
+def classifyNB(vec2Classify,p0Vect,p1Vect,pClass1):
+    p1=sum(vec2Classify * p1Vect) + log(pClass1)
+    p0=sum(vec2Classify * p0Vect) + log(1.0 - pClass1)
+    if p1>p0:
+        return 1
+    else:
+        return 0
+
+def textParse(bigString):
+    import re
+    listOfTokens=re.split(r'\w*',bigString)
+    return [tok.lower() for tok in listOfTokens if len(tok) > 2]
+
+def spamTest():
+    docList=[]
+    classList=[]
+    fullText=[]
+    for i in range(1,26):
+        wordList=textParse(open('email/spam/%d.txt'%i).read())
+        
--- testBayes.py
+++ testBayes.py
@@ -1,7 +1,7 @@
 import bayes
 postingList,classList=bayes.loadDataSet()
 myVocabList=bayes.createVocabList(postingList)
-print(postingList)
+print(myVocabList)
 #myVocabVec=bayes.setOfWord2Vec(myVocabList,postingList[0])
 #print(myVocabVec)
 trainMat=[]
@@ -9,6 +9,13 @@ for postInDoc in postingList:
     trainMat.append(bayes.setOfWord2Vec(myVocabList,postInDoc))
 #print(trainMat)
 p0Vect,p1Vect,pAbusive=bayes.trainNB0(trainMat,classList)
-print(p0Vect)
-print(p1Vect)
-print(pAbusive)
+#print(p0Vect)
+#print(p1Vect)
+#print(pAbusive)
+testEntry=['love','my','dalmation']
+thisDoc=bayes.setOfWord2Vec(myVocabList,testEntry)
+print('{} classified as:{}'.format(testEntry,bayes.classifyNB(thisDoc,p0Vect,p1Vect,pAbusive)))
+testEntry=['stupid','garbage']
+thisDoc=bayes.setOfWord2Vec(myVocabList,testEntry)
+print('{} classified as:{}'.format(testEntry,bayes.classifyNB(thisDoc,p0Vect,p1Vect,pAbusive)))
+
